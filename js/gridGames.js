e1722cc3c8f9987006ff4d286c76b780f2d0acff
FavioB-Win <faviobranezlol@gmail.com>
2026-09-08 20:35:01 -0500

    refactor: cambiar el llamado a la api, ahora asyc y awai

--- js/gridGames.js
+++ js/gridGames.js
@@ -1,15 +1,15 @@
 const gridGames = document.getElementById("games-grid");
 
-const loadGames = () => {
+async function loadGames() {
     gridGames.innerHTML = ""
 
-    fetch("juegos.json")
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(game => {
-                const gameCard = document.createElement("article");
-                gameCard.classList.add("game-card");
-                gameCard.innerHTML = `
+    const response = await fetch("juegos.json");
+    const data = await response.json();
+
+    data.forEach(game => {
+        const gameCard = document.createElement("article");
+        gameCard.classList.add("game-card");
+        gameCard.innerHTML = `
                     <div class="game-poster">
                     <img src="${game.media.cover_url}" alt="Poster de ${game.title}" />
                     </div>
@@ -36,9 +36,8 @@ const loadGames = () => {
                         <button class="btn-primary">OBTENER</button>
                     </div>
                 `;
-                gridGames.appendChild(gameCard);
-            });
-        });
+        gridGames.appendChild(gameCard);
+    });
 };
 
 loadGames();
