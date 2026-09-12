d4402e731f02e077177bd217e69f3f8235150cc9
FavioB-CachyOs <faviobranezlol@gmail.com>
2026-09-11 21:40:42 -0500

    feat: aplicar script para juegos en oferta como maximo 4 juego dinámicamente

--- js/saleGames.js
+++ js/saleGames.js
@@ -1,21 +1,38 @@
-const featGames = document.querySelectorAll('.sale-games-grid');
+const saleGames = document.getElementById('sale-games-grid');
 
-async function loadFeatGames() {
+async function loadSaleGames() {
     const response = await fetch("juegos.json");
     const data = await response.json();
 
-    data.forEach(game => {
-        const flashCard = document.createElement("article");
-        flashCard.classList.add("flash-card");
+    data.slice(5,9).forEach(game => {
+        const flashCard = document.createElement('article');
+        flashCard.classList.add('flash-card');
         flashCard.innerHTML = `
             <div class="flash-card">
                 <img src=${game.media?.wide_cover_url} alt=${game.title}/>
-                <div class="flash-badge-platform">${game.publisher} ${game.platforms[0]}</div>
-                <div class="flash-badge-discount">-${game.prices.discount_percentage}%</div>
-            </div> 
+                <div class="flash-badge-platform">${game.publisher}/ ${game.platforms?.[0]}</div>
+                <div class="flash-badge-discount">-${game.prices?.discount_percentage}%</div>
+            </div>
+            <div class="flash-stock-bar">
+                <div class="stock-progress" style="width: ${game.stock.percentage}%"></div>
+            </div>
+            <div class="flash-info">
+                <div class="flash-meta">
+                    <span class="edition">${game.edition}</span>
+                    <span class="keys-left">Quedan ${game.stock.available} clave(s)</span>
+                </div>
+                <h3 class="flash-game-tittle">${game.title}</h3>
+            </div>
+            <div class="flash-footer">
+                <div class="flash-prices">
+                    <span class="old-price">${game.prices?.original_price}</span>
+                    <span class="new-price"><span class="text-green">$${game.prices?.current_price}</span> <small>${game.prices?.currency}</small> </span> 
+                </div>
+                <button class="btn-buy">🛒 COMPRAR</button>
+            </div>
         `;
-        featGames.appendChild(flashCard);
+        saleGames.appendChild(flashCard);
     });
 }
 
-loadFeatGames().catch(error => console.error("Error al cargar los juegos con descuento:", error));
+loadSaleGames().catch(error => console.error("Error al cargar los juegos con descuento:", error));
